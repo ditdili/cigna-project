47e9a7da9116748c5b14f9ef5e4ffc3d2b5aa9dc
ditdili <aliyevmaqsud@gmail.com>
2021-08-30 18:42:58 -0500

    Added useEffect to ItemList to load the data from server.
    Added tests with mock fn to check the data loaded and displayed.

--- client/src/__tests__/ItemList.test.js
+++ client/src/__tests__/ItemList.test.js
@@ -1,19 +1,55 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
 import { ItemList } from '../components/ItemList';
 import { StoreProvider } from '../features/store';
 
+const returnValues = {
+  results: [
+    {
+      name: 'Louis Pasteur',
+      specialty: 'Molecular Asymmetry',
+    },
+    {
+      name: 'Dr. Jean-Martin Charcot',
+      specialty: 'Pathology',
+    },
+  ],
+};
+
 describe('ItemList component', () => {
-  test('should have a table with Name and Specialty columns', () => {
+  beforeEach(() => {
+    jest.spyOn(window, 'fetch');
+
     render(
       <StoreProvider>
         <ItemList />
       </StoreProvider>
     );
+
+    window.fetch.mockReturnValue(returnValues);
+  });
+
+  afterEach(() => jest.restoreAllMocks());
+
+  test('should have a table with Name and Specialty columns', () => {
     const table = screen.getByTestId('table');
     expect(table).toBeInTheDocument();
 
     const [columnNames] = within(table).getAllByRole('rowgroup');
     expect(within(columnNames).getByText(/name/i)).toBeInTheDocument();
     expect(within(columnNames).getByText(/specialty/i)).toBeInTheDocument();
+  });
+
+  test('should call the right api', () => {
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost:5000/individuals'
+    );
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test('should return a table body with rows and data provided', async () => {
+    const { results } = returnValues;
+
+    const row = await waitFor(() => screen.getByTestId('row-0'));
+    expect(within(row).getByText(results[0].name)).toBeInTheDocument();
   });
 });
--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -4,6 +4,21 @@ import { useStore } from '../features/store';
 export const ItemList = () => {
   const { list, setList } = useStore();
 
+  useEffect(() => {
+    let mounted = true;
+    const getIndividuals = async () => {
+      const response = await fetch('http://localhost:5000/individuals');
+      const { results } = await response.json();
+
+      if (mounted) {
+        setList(results);
+      }
+    };
+
+    getIndividuals();
+    return () => (mounted = false);
+  }, [setList]);
+
   return (
     <>
       <div className="row">
@@ -16,10 +31,14 @@ export const ItemList = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Test Name</td>
-                <td>Test Specialty</td>
-              </tr>
+              {list.map((individual, index) => {
+                return (
+                  <tr key={index} data-testid={`row-${index}`}>
+                    <td>{individual.name}</td>
+                    <td>{individual.specialty}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
